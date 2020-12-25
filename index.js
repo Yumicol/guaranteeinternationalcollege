dc745667927b4616106d66dfcd29a4893510603f
WatermelonPoison <william_khine@outlook.com>
2020-12-25 19:16:11 +0630

    git test

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    res.send('home');
+    res.render('home');
 })
 
-var port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`LISTENING ON PORT ${port}`);
 })
